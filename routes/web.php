0373a51d39843b320cdb1d5fbce00090e832f47c
Gludovatz Attila <attila@gludovatz.hu>
2022-10-23 22:26:46 +0200

    Middleware Alapok

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Middleware\CheckHour;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/goodnight', function () {
+    return "Jó éjszakát!";
+})->middleware(['check-hour:20']);
