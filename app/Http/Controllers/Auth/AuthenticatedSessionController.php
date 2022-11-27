a7d3e8e4453da909d2e58fffd46f1524ee93a4d1
Gludovatz Attila <attila@gludovatz.hu>
2022-11-27 22:34:38 +0100

    Breeze: új funkció (profile) és megjelenítés (dark mode)

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        // return redirect('test-login');
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
